b2f2c5ddabe0046f4857d662980d2e433a906d97
Bruno Correa <correaricardobruno@gmail.com>
2024-04-18 19:08:51 -0300

    add test for authenticable interface

--- src/AuditorRH.java
+++ src/AuditorRH.java
@@ -6,4 +6,20 @@ public class AuditorRH implements Autenticavel {
     public boolean autentica (String senha) {
         return this.senha.equals(senha);
     }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 }
---
+++ src/TesteAutenticacavel.java
@@ -0,0 +1,12 @@
+public class TesteAutenticacavel {
+    public static void main(String[] args) {
+           AuditorRH auditorRH = new AuditorRH();
+           auditorRH.setSenha("4567");
+
+           if(auditorRH.autentica("4567")) {
+               System.out.println("Auditor autenticado com sucesso");
+           } else {
+               System.out.println("Falha ao autenticar, senha incorreta");
+           }
+    }
+}
